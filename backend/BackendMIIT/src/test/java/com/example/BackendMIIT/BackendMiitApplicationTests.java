e18c01fe2fb22141bcd7b65539abe745c472f2de
nn17 <yakor356@gmail.com>
2024-09-19 00:54:33 +0300

    Spring initial commit

---
+++ backend/BackendMIIT/src/test/java/com/example/BackendMIIT/BackendMiitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendMIIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
