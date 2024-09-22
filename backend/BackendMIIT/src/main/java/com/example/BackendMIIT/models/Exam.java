191507a7e481fa68eb2b08471d1afc5ea36f68eb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-22 20:24:29 +0300

    feat: add the exam entity

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/models/Exam.java
@@ -0,0 +1,26 @@
+package com.example.BackendMIIT.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "exams")
+public class Exam extends BaseEntity {
+    private String name;
+
+    protected Exam() {}
+
+    public Exam(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "exam_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
