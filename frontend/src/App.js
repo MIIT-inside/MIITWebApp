06f7456328fed80a6edc998a6032136de6982a72
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-09-23 01:04:00 +0300

    Add header template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <Header />
+      </div>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header--container">
+                <div className="header--logo">
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
