3b77c9f6f3fda6bee5f6c54ef2235a71f8384eed
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-22 20:21:38 +0300

    feat: add base entity

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/models/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.example.BackendMIIT.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    protected Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+}
