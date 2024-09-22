a5fdcaab72288479569d92679f726c33750081cd
nn17 <yakor356@gmail.com>
2024-09-23 00:09:17 +0300

    feature: add profile dto

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/models/dto/ProfileDto.java
@@ -0,0 +1,11 @@
+package com.example.BackendMIIT.models.dto;
+
+import lombok.Data;
+
+@Data
+public class ProfileDto {
+
+    private String name;
+    private int passPoint;
+    private String description;
+}
