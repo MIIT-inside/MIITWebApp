6d41f2141d4de6d5171686041ea345ddb7179190
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-09-24 01:57:06 +0300

    Add images in main-section with needed styles

--- frontend/src/components/MainSection.js
+++ frontend/src/components/MainSection.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/MainSection.css';
 import Header from './Header';
+import LeftImage from '../assets/RUT_logo.svg';
+import RightImage from '../assets/RUT_logo.svg';
 
 const MainSection = () => {
     return (
         <div className="main-section">
             <Header />
+            <img src={LeftImage} alt="Left Background" className="background-image-left" />
+            <img src={RightImage} alt="Right Background" className="background-image-right" />
             <div className="main-content">
                 <h1>Абитуриенту</h1>
                 <div className="search-block">
