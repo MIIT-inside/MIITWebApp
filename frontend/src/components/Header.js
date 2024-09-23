8e671b1cc6b93c760cf3f8020da1b81b140aa17e
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-09-24 02:18:26 +0300

    Add header-logo in header with needed parameters

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/Header.css';
+import HeaderLogo from "../assets/header_logo.svg";
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header--container">
                 <div className="header--logo">
+                    <img src={HeaderLogo} alt="Header logo"/>
                 </div>
             </div>
         </header>
