4535594b8405549aa9101508d78977ca79460632
nn17 <yakor356@gmail.com>
2024-09-30 00:10:40 +0300

    feature: add profile controller

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/ProfileController.java
@@ -0,0 +1,28 @@
+package com.example.BackendMIIT.controller;
+
+import com.example.BackendMIIT.model.dto.ProfileDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/miit/profiles")
+public class ProfileController {
+
+    @GetMapping
+    public ResponseEntity<List<ProfileDto>> getAllProfiles() {
+        return ResponseEntity.ok().body(new ArrayList<>());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProfileDto>> getProfilesByPassPoint(@PathVariable int passPoint) {
+        return ResponseEntity.ok().body(new ArrayList<>());
+    }
+
+    @GetMapping
+    public ResponseEntity<ProfileDto> getProfileByName(@PathVariable String name) {
+        return ResponseEntity.ok().body(new ProfileDto());
+    }
+}
