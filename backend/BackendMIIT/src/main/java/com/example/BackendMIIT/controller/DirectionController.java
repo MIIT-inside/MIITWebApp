976f7c715f65836e1dc73ffa75ba75bdef198244
nn17 <yakor356@gmail.com>
2024-09-30 00:02:53 +0300

    feature: add direction controller

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/DirectionController.java
@@ -0,0 +1,28 @@
+package com.example.BackendMIIT.controller;
+
+import com.example.BackendMIIT.model.dto.DirectionDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/miit/directions")
+public class DirectionController {
+
+    @GetMapping
+    public ResponseEntity<List<DirectionDto>> getAllDirections() {
+        return ResponseEntity.ok().body(new ArrayList<>());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DirectionDto>> getDirectionsByPassPoint(@PathVariable int passPoint) {
+        return ResponseEntity.ok().body(new ArrayList<>());
+    }
+
+    @GetMapping
+    public ResponseEntity<DirectionDto> getDirectionByName(@RequestParam String directionName) {
+        return ResponseEntity.ok().body(new DirectionDto());
+    }
+}
