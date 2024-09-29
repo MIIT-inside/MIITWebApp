866bc309d4307e7eaf574ec4f342e3357cbb129c
nn17 <yakor356@gmail.com>
2024-09-29 22:23:27 +0300

    feature: add get mapping for educator

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/EducatorController.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/EducatorController.java
@@ -1,10 +1,26 @@
 package com.example.BackendMIIT.controller;
 
+import com.example.BackendMIIT.model.domain.Educator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/miit/educators")
 public class EducatorController {
 
+    @GetMapping
+    public ResponseEntity<List<Educator>> getAllEducators() {
+        return ResponseEntity.ok().body(new ArrayList<>());
+    }
+
+    @GetMapping
+    public ResponseEntity<Educator> getEducatorsByDepartmentName(@RequestParam String departmentName) {
+        return ResponseEntity.ok().body(new Educator());
+    }
 }
