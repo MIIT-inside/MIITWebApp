f7aaa98524fed167cbc3916fcc4c0b4a593216be
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-10-01 01:46:12 +0300

    write styles and change jsx for TitleWithLine.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MainSection from "./components/MainSection";
+import TitleWithLine from "./components/TitleWithLine";
 
 function App() {
   return (
       <div className="App">
         <MainSection />
+          <TitleWithLine/>
       </div>
   );
 }
--- frontend/src/components/TitleWithLine.jsx
+++ frontend/src/components/TitleWithLine.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './TitleWithLine.css';
+import '../styles/TitleWithLine.css';
 
 const TitleWithLine = ({ title }) => {
     return (
         <div className="title-with-line">
-            <h2>{title}</h2>
-            <hr className="title-line" />
+            <h2>{'title'}</h2>
+            <div className="line"></div>
         </div>
     );
 };
