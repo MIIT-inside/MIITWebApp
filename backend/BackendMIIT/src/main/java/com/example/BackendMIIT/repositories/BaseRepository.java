b82518eda819d120588e4bd9cb9857e46837a148
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-30 08:39:54 +0300

    feat: create the base repository

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repositories/BaseRepository.java
@@ -0,0 +1,9 @@
+package com.example.BackendMIIT.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+
+}
