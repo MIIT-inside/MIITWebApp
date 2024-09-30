1f4f944db8db58e290c48e2a081e3e450e088d28
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-30 08:40:54 +0300

    feat: create repository for the department

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repositories/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.example.BackendMIIT.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository<T, ID> extends BaseRepository<T, ID>{
+
+}
