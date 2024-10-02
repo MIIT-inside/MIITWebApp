9fbcedbfb97fcdc79a55f86d4ae60098e3aa17d8
nn17 <yakor356@gmail.com>
2024-10-03 01:49:08 +0300

    feature: add direction service

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/DirectionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.BackendMIIT.service.impl;
+
+import com.example.BackendMIIT.service.DirectionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DirectionServiceImpl implements DirectionService {
+}
