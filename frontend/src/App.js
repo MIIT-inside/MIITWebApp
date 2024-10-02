98f31359674cafc734e18ddf8e2aa095cd7767fb
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-10-03 00:08:22 +0300

    Create Carousel template for individual-achievements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MainSection from "./components/MainSection";
 import InstructionSteps from "./components/InstructionSteps";
 import TitleWithLine from "./components/TitleWithLine";
+import {Carousel} from "./carousel/Carousel";
 import React from "react";
 
 function App() {
@@ -11,6 +12,11 @@ function App() {
           <TitleWithLine title="Как поступить" />
           <InstructionSteps/>
           <TitleWithLine title="Баллы за индивидуальные достижения" />
+          <Carousel>
+              <div className="item item-1">Item 1</div>
+              <div className="item item-2">Item 2</div>
+              <div className="item item-3">Item 3</div>
+          </Carousel>
       </div>
   );
 }
