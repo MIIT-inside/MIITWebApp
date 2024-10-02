5f6601bd9965953838ae8054b8828b32be0856cc
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-10-03 00:19:22 +0300

    add data.js

---
+++ frontend/src/carousel/Carousel.js
@@ -0,0 +1,59 @@
+import { useEffect, useState, Children, cloneElement } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import './Carousel.css'
+
+const PAGE_WIDTH = 450
+
+export const Carousel = ({ children }) => {
+    const [pages, setPages] = useState([])
+    const [offset, setOffset] = useState(0)
+
+    const handleLeftArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset + PAGE_WIDTH
+            console.log(newOffset)
+            return Math.min(newOffset, 0)
+        })
+    }
+    const handleRightArrowClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset - PAGE_WIDTH
+
+            const maxOffset = -(PAGE_WIDTH * (pages.length - 1))
+
+            console.log(newOffset, maxOffset)
+            return Math.max(newOffset, maxOffset)
+        })
+    }
+
+    useEffect(() => {
+        setPages(
+            Children.map(children, (child) => {
+                return cloneElement(child, {
+                    style: {
+                        minWidth: `${PAGE_WIDTH}px`,
+                        maxWidth: `${PAGE_WIDTH}px`,
+                        height: '100%',
+                    },
+                })
+            })
+        )
+    }, [])
+
+    return (
+        <div className="main-container">
+            <FaChevronLeft className="arrow" onClick={handleLeftArrowClick} />
+            <div className="window">
+                <div
+                    className="all-pages-container"
+                    style={{
+                        transform: `translateX(${offset}px)`,
+                    }}
+                >
+                    {pages}
+                </div>
+            </div>
+            <FaChevronRight className="arrow" onClick={handleRightArrowClick} />
+        </div>
+    )
+}
