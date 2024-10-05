7f99415dcc5b1e0e11a2109b056a2ae1ed15adb9
nn17 <yakor356@gmail.com>
2024-10-06 02:14:56 +0300

    fix: add base url to app properties

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/AppConfig.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.BackendMIIT.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
+    @Value("${webclient.baseurl")
+    private String baseUrl;
+
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().baseUrl("https://localhost:8080").build();
+        return WebClient.builder().baseUrl(baseUrl).build();
     }
 }
