d9059cb2b3a29263a0efe3fdfd319a6d8155620c
nn17 <yakor356@gmail.com>
2024-10-06 01:44:28 +0300

    fix: ci test

--- backend/BackendMIIT/src/test/java/com/example/BackendMIIT/controller/ProfileControllerTest.java
+++ backend/BackendMIIT/src/test/java/com/example/BackendMIIT/controller/ProfileControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.BackendMIIT.controller;
 
+import com.example.BackendMIIT.service.ProfileService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = ProfileController.class)
@@ -13,6 +15,9 @@ public class ProfileControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ProfileService profileService;
 
     @Test
     public void getAllProfiles() throws Exception {
