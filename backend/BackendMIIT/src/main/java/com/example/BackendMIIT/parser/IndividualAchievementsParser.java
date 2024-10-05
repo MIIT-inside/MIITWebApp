487a0b1add9885bd0b707067e6d7afd305652dcd
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-05 18:21:04 +0300

    feat: create an achievement parser

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/parser/IndividualAchievementsParser.java
@@ -0,0 +1,53 @@
+package com.example.BackendMIIT.parser;
+
+import com.example.BackendMIIT.model.domain.IndividualAchievements;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IndividualAchievementsParser {
+    private static final String URL = "https://miit.ru/page/136668";
+
+    public Set<IndividualAchievements> parse() {
+        Set<IndividualAchievements> achievements = new HashSet<>();
+
+        try {
+            Document document = Jsoup.connect(URL).get();
+            Elements achievementsElements  = document.select("tr");
+
+            for (Element achievement: achievementsElements) {
+                IndividualAchievements individualAchievement = extractAchievement(achievement);
+
+                if (individualAchievement != null) {
+                    achievements.add(individualAchievement);
+                }
+            }
+        } catch (IOException | NumberFormatException ioException) {
+            System.err.println(ioException.getMessage());
+        }
+
+        return achievements;
+    }
+
+    private IndividualAchievements extractAchievement(Element achievement) {
+        try {
+            String description = achievement.select("td:nth-child(2)").text().trim();
+            int countPoints = Integer.parseInt(achievement.select("td:nth-child(3)").text().trim());
+
+            IndividualAchievements individualAchievements = new IndividualAchievements();
+            individualAchievements.setDescription(description);
+            individualAchievements.setCountPoints(countPoints);
+
+            return individualAchievements;
+        } catch (NumberFormatException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+}
