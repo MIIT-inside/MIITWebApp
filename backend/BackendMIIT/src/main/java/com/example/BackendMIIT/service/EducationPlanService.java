b798a90c3f0a10c7031c8c309e95e97b28a14eea
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-06 20:54:49 +0300

    chore: rename the method

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/EducationPlanService.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/EducationPlanService.java
@@ -5,5 +5,5 @@ import com.example.BackendMIIT.model.domain.Profile;
 import java.util.List;
 
 public interface EducationPlanService {
-    void parseEducationPlans(List<Profile> profiles);
+    void parseAndSaveEducationPlans(List<Profile> profiles);
 }
