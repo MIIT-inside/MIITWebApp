5f7012dddf028cf0036b401a6b98d0154130ef56
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-06 20:55:06 +0300

    feat: implement the education plan service

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/EducationPlanServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.BackendMIIT.service.impl;
+
+import com.example.BackendMIIT.model.domain.Profile;
+import com.example.BackendMIIT.parser.EducationPlanParser;
+import com.example.BackendMIIT.repositories.ProfileRepository;
+import com.example.BackendMIIT.service.EducationPlanService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EducationPlanServiceImpl implements EducationPlanService {
+
+    private final ProfileRepository profileRepository;
+    private final EducationPlanParser parser;
+
+    public EducationPlanServiceImpl(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+        this.parser = new EducationPlanParser();
+    }
+
+    @Override
+    @Transactional
+    public void parseAndSaveEducationPlans(List<Profile> profiles) {
+        List<Profile> updatedProfiles = parser.parseEducationPlan(profiles);
+        profileRepository.saveAll(updatedProfiles);
+    }
+}
