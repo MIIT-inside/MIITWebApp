3251166a1f8ec962c30f85e5c34ec4eb17f25e40
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-13 23:10:07 +0300

    feat: create the discipline parser

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/parser/DisciplineParser.java
@@ -0,0 +1,41 @@
+package com.example.BackendMIIT.parser;
+
+import com.example.BackendMIIT.model.domain.Discipline;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisciplineParser {
+
+    private final LessonParser lessonParser;
+
+    public DisciplineParser(LessonParser lessonParser) {
+        this.lessonParser = lessonParser;
+    }
+
+    public List<Discipline> parseDisciplines(Element semesterElement) {
+        List<Discipline> disciplines = new ArrayList<>();
+        Elements disciplineElements = semesterElement.select("h4.eduplan_discipline-header");
+
+        for (Element disciplineElement : disciplineElements) {
+            String disciplineName = disciplineElement.text();
+            Discipline discipline = new Discipline();
+            discipline.setName(disciplineName);
+            discipline.setLessons(lessonParser.parseLessons(disciplineElement));
+
+            String attestationCSS = "div.eduplan_discipline-content-header:contains(Форма промежуточной аттестации) + ul li";
+            Element attestationElement = disciplineElement.parent().selectFirst(attestationCSS);
+
+            if (attestationElement != null) {
+                discipline.setAttestation(attestationElement.text().trim());
+            }
+
+            disciplines.add(discipline);
+        }
+
+        return disciplines;
+    }
+}
+
