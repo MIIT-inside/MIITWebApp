de6deb0dd04dfcb3682229b2aba67522172e5a6d
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-14 22:29:36 +0300

    fix: remove findByPoints method because the passPoint field doesn't exist

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repositories/ProfileRepository.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repositories/ProfileRepository.java
@@ -3,12 +3,9 @@ package com.example.BackendMIIT.repositories;
 import com.example.BackendMIIT.model.domain.Profile;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends BaseRepository<Profile> {
-    List<Profile> findByPassPoint(int passPoint);
-
     Optional<Profile> findByName(String name);
 }
