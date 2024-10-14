13ab8e3b061ddf93fa770e47941a838829c91a3c
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-14 22:30:19 +0300

    chore: remove unused imports

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/EducationPlanService.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/EducationPlanService.java
@@ -1,9 +1,5 @@
 package com.example.BackendMIIT.service;
 
-import com.example.BackendMIIT.model.domain.Profile;
-
-import java.util.List;
-
 public interface EducationPlanService {
     void parseAndSaveEducationPlans();
 }
