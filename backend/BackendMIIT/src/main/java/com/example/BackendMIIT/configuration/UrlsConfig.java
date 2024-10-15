1399c911fb21bfd3c20ad691a1e6b50fa963c989
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-15 22:11:24 +0300

    refactor: replace the component annotation with a configuration annotation

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/UrlsConfig.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/UrlsConfig.java
@@ -3,11 +3,12 @@ package com.example.BackendMIIT.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "url")
 public class UrlsConfig {
 
