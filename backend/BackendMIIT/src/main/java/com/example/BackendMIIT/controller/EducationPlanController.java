344e73d2792c70b7e80f487b9fc0d2bb8fb16da4
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-17 20:23:51 +0300

    chore: edit the end-point path

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/EducationPlanController.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/controller/EducationPlanController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/miit/education-plans")
+@RequestMapping(path = "api/miit/plans")
 public class EducationPlanController {
     private final EducationPlanService educationPlanService;
 
