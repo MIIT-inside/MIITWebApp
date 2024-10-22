d5f90918e6c768b2e0dce773022a1d5294f732dc
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-10-22 21:27:09 +0300

    feat: add header and main image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
 
 export default function App() {
-    return (<div></div>);
+    return (<div>
+        <Header/>
+        <MainContent/>
+    </div>);
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="flex bg-white w-full px-16 py-7 justify-between items-center">
+            <div className="text-3xl text-[#3A5BCC] font-bold">РУТ</div>
+            <div className="text-[#1E1E1E] space-x-5">
+                <span>Специальности</span>
+                <span>Профориентация</span>
+                <span>Часто задаваемые воопросы</span>
+            </div>
+            <button className="px-5 py-2 bg-[#3A5BCC] text-white">Поиск</button>
+        </div>
+    )
+}
