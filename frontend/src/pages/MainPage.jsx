e727472434188d0031b254948a29d23b46da9382
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-10-25 14:30:28 +0300

    feat: add fast animation

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from "../components/Header";
 import InfoBlock from "../components/InfoBlock";
-import Dropdown from "../components/Accordion";
 import Accordion from "../components/Accordion";
 import image1 from "../assets/tenweb_media_zhurFxyM.webp"
 import BlueCard from "../components/BlueCard";
+import {motion} from "framer-motion";
 
 export default function MainPage() {
     return (
@@ -12,7 +12,10 @@ export default function MainPage() {
             <Header/>
             <div className="h-screen relative bg-[url('./src/assets/image_2024-10-22_12-22-33.png')] bg-fixed bg-cover">
                 <div className="absolute inset-0 bg-black opacity-60"></div>
-                <div
+                <motion.div
+                    initial={{opacity: 0, y: 50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 1, ease: "easeOut"}}
                     className="relative flex flex-col w-[47%] m-auto space-y-6 items-center justify-center size-full border-0 rounded-xl pt-16">
                     <span className="flex text-center text-4xl text-white font-bold">Добро пожаловать в РУТ МИИТ</span>
                     <span className="text-center text-xl text-white">Откройте для себя возможности, которые предлагает наш университет. Узнайте больше о наших специальностях и карьерных перспективах.</span>
@@ -20,9 +23,13 @@ export default function MainPage() {
                         <button className="bg-white px-5 py-3 rounded">Подробнее</button>
                         <button className="bg-none px-5 py-3 border rounded text-white">Контакты</button>
                     </div>
-                </div>
+                </motion.div>
             </div>
-            <div className="flex justify-between mt-16 mx-auto max-w-[1280px]">
+            <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 1, ease: "easeOut"}}
+                className="flex justify-between mt-16 mx-auto max-w-[1280px]">
                 <span className="text-4xl font-bold">Обзор наших курсов</span>
                 <div className="flex flex-col w-[50%] space-y-7">
                     <span className="text-xl text-[#1E1E1E]">Наши курсы предлагают уникальные возможности для студентов, стремящихся к успешной карьере. Узнайте больше о каждом курсе, его учебной программе и карьерных перспективах.</span>
@@ -33,7 +40,7 @@ export default function MainPage() {
                                    description="Подробные описания курсов и их значимость на рынке труда."/>
                     </div>
                 </div>
-            </div>
+            </motion.div>
             <div className="flex flex-col mt-20 justify-center items-center space-y-5 mx-auto max-w-[1280px]">
                 <span className="text-4xl font-bold">Навигация по курсам</span>
                 <span className="text-xl text-[#1E1E1E]">Выберите факультет, чтобы увидеть доступные курсы.</span>
