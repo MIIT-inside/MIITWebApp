f079db377fba2a2b071a76fda433823ec67313d1
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-28 19:46:37 +0300

    feat: add requests to sort directions

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repository/DirectionRepository.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repository/DirectionRepository.java
@@ -1,8 +1,10 @@
 package com.example.BackendMIIT.repository;
 
 import com.example.BackendMIIT.model.domain.Direction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,4 +12,10 @@ public interface DirectionRepository extends BaseRepository<Direction> {
     Optional<Direction> findByName(String name);
 
     Optional<Direction> findByCode(String code);
+
+    @Query("SELECT d FROM Direction d LEFT JOIN d.passPoints p GROUP BY d ORDER BY p.avg DESC, d.name ASC")
+    List<Direction> findAllOrderByAvgPassPointAndName();
+
+    @Query("SELECT d FROM Direction d LEFT JOIN d.passPoints p GROUP BY d ORDER BY p.min DESC, d.name ASC")
+    List<Direction> findAllOrderByMinPassPointAndName();
 }
