b3288fd4010309648e1d206ee93ba3e67ddaedfb
nn17 <yakor356@gmail.com>
2024-10-28 20:12:04 +0300

    feature: add redis config

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/BackendMiitApplication.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/BackendMiitApplication.java
@@ -2,8 +2,10 @@ package com.example.BackendMIIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BackendMiitApplication {
 
     public static void main(String[] args) {
