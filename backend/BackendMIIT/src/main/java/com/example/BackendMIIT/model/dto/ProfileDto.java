838bc5c6f80118f5fd86acdafde0314bbe7f33aa
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-29 00:16:19 +0300

    feat: don't show null fields

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/model/dto/ProfileDto.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/model/dto/ProfileDto.java
@@ -1,9 +1,11 @@
 package com.example.BackendMIIT.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProfileDto {
 
     @JsonProperty("name")
