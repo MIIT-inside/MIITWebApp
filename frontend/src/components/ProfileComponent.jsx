77922d0cdb430fe549f41753014c490784e86780
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-11-03 01:41:06 +0300

    feat: add profile props and styles

--- frontend/src/components/ProfileComponent.jsx
+++ frontend/src/components/ProfileComponent.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function ProfileComponent() {
+export default function ProfileComponent({image, name, description}) {
     return (
-        <div>
-
+        <div className="flex flex-col space-y-3">
+            <img src={image} alt="изображение профиля"/>
+            <span className="text-xl font-bold">{name}</span>
+            <p>{description}</p>
+            <button className="px-5 py-3 bg-none border border-black">Узнать больше</button>
         </div>
     )
 }
--- frontend/src/pages/SpecializationPage.jsx
+++ frontend/src/pages/SpecializationPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProfileComponent from "../components/ProfileComponent";
 
 export default function SpecializationPage() {
     return (
@@ -6,6 +7,12 @@ export default function SpecializationPage() {
             <div className="flex items-center justify-between">
                 <span className="text-3xl font-bold">Профили подготовки</span>
                 <span className="w-1/2">На этой странице представлены образовательные программы университета, каждая из которых открывает возможности для профессионального роста. Изучите доступные направления, чтобы понять, какие знания и навыки можно получить, и сделать уверенный шаг к успешной карьере.</span>
+            </div>
+            <div className="flex mt-20 grid-cols-3 space-x-5">
+                <ProfileComponent name="Технологии разработки программного обеспечения" description="empty"/>
+                <ProfileComponent name="Технологии разработки программного обеспечения" description="empty"/>
+                <ProfileComponent name="Технологии разработки программного обеспечения" description="empty"/>
+                <ProfileComponent name="Технологии разработки программного обеспечения" description="empty"/>
             </div>
         </div>
     )
