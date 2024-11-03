bed639ba920dca3f31130138ac09a9f22d72fc46
nn17 <yakor356@gmail.com>
2024-11-03 23:20:50 +0300

    feature: add global cors config

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.BackendMIIT.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/api/miit")
+				.allowedOrigins("http://localhost:8080")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*");
+	}
+}
