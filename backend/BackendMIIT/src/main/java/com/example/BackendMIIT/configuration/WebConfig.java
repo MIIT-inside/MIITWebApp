fddffc00752be8dceeadd25479b4e265bb2d3569
nn17 <yakor356@gmail.com>
2024-11-05 01:57:57 +0300

    chore: change cors config and dockerfile

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/WebConfig.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/configuration/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
-				.addMapping("/api/miit")
+				.addMapping("/**")
 				.allowedOrigins("http://localhost:8080")
 				.allowedMethods("GET", "POST", "PUT", "DELETE")
 				.allowedHeaders("*");
