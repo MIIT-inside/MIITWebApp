ace55572f63ad2a670fdcd85e64851e2c20f7a9b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-08 16:06:44 +0300

    refactor: remove the unnecessary annotation

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/model/dto/ProfileDto.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/model/dto/ProfileDto.java
@@ -1,11 +1,9 @@
 package com.example.BackendMIIT.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProfileDto {
 
     @JsonProperty("name")
