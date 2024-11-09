bfee63765126a11ee4a9ac4b9f974ea950bb3a3f
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-09 23:14:54 +0300

    refactor: replace the return type from the entity to the dto

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/ExamService.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/ExamService.java
@@ -1,11 +1,11 @@
 package com.example.BackendMIIT.service;
 
-import com.example.BackendMIIT.model.domain.Exam;
+import com.example.BackendMIIT.model.dto.ExamDto;
 
 import java.util.List;
 
 public interface ExamService {
     void parseAndSaveExamSubjects();
 
-    List<Exam> getAllExams();
+    List<ExamDto> getAllExams();
 }
--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/ExamServiceImpl.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/ExamServiceImpl.java
@@ -1,20 +1,25 @@
 package com.example.BackendMIIT.service.impl;
 
+import com.example.BackendMIIT.mapper.ExamMapper;
 import com.example.BackendMIIT.model.domain.Exam;
+import com.example.BackendMIIT.model.dto.ExamDto;
 import com.example.BackendMIIT.parser.ExamParser;
 import com.example.BackendMIIT.repository.ExamRepository;
 import com.example.BackendMIIT.service.ExamService;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ExamServiceImpl implements ExamService {
 
     private final ExamRepository examRepository;
+    private final ExamMapper examMapper;
     private final ExamParser parser;
 
-    public ExamServiceImpl(ExamRepository examRepository, ExamParser parser) {
+    public ExamServiceImpl(ExamRepository examRepository, ExamMapper examMapper, ExamParser parser) {
         this.examRepository = examRepository;
+        this.examMapper = examMapper;
         this.parser = parser;
     }
 
@@ -25,7 +30,10 @@ public class ExamServiceImpl implements ExamService {
     }
 
     @Override
-    public List<Exam> getAllExams() {
-        return examRepository.findAll();
+    public List<ExamDto> getAllExams() {
+        return examRepository.findAll()
+                             .stream()
+                             .map(examMapper::examToDto)
+                             .collect(Collectors.toList());
     }
 }
