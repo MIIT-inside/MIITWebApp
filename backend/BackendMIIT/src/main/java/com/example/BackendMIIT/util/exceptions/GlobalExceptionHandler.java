b1b0d9b7d204c6554b7b07dc57df66ac5bc95d5b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-10 16:20:48 +0300

    feat: handle the direction not found exception by the global exception handler

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/util/exceptions/GlobalExceptionHandler.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/util/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.example.BackendMIIT.util.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(DirectionNotFoundException.class)
+    public ResponseEntity<String> handleDirectionNotFoundException(DirectionNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Введите название направления");
+    }
 }
