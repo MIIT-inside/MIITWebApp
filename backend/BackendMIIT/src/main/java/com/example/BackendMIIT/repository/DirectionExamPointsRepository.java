3fb6a50cbf9bbdc147ffd053ae3219fad7e1bba8
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-10 15:40:10 +0300

    feat: create repository for the DirectionExamPoints entity

---
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repository/DirectionExamPointsRepository.java
@@ -0,0 +1,7 @@
+package com.example.BackendMIIT.repository;
+
+import com.example.BackendMIIT.model.domain.DirectionExamPoints;
+
+public interface DirectionExamPointsRepository extends BaseRepository<DirectionExamPoints> {
+
+}
