98a704e2a7459917d8551166cfc61abdbabc891d
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-10 16:03:18 +0300

    feat: annotate the interface as @Repository and add a findByDirection method

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repository/DirectionExamPointsRepository.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/repository/DirectionExamPointsRepository.java
@@ -1,7 +1,12 @@
 package com.example.BackendMIIT.repository;
 
+import com.example.BackendMIIT.model.domain.Direction;
 import com.example.BackendMIIT.model.domain.DirectionExamPoints;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DirectionExamPointsRepository extends BaseRepository<DirectionExamPoints> {
-
+    List<DirectionExamPoints> findByDirection(Direction direction);
 }
