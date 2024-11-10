f80b135990581949c4e63438e41f3ce2840c97ac
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-10 15:48:00 +0300

    feat: implement parsing min points method and save the min points to the db

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/ExamServiceImpl.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/service/impl/ExamServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.BackendMIIT.service.impl;
 
 import com.example.BackendMIIT.mapper.ExamMapper;
+import com.example.BackendMIIT.model.domain.Direction;
+import com.example.BackendMIIT.model.domain.DirectionExamPoints;
 import com.example.BackendMIIT.model.domain.Exam;
 import com.example.BackendMIIT.model.dto.ExamDto;
+import com.example.BackendMIIT.parser.DirectionExamPointsParser;
 import com.example.BackendMIIT.parser.ExamParser;
+import com.example.BackendMIIT.repository.DirectionExamPointsRepository;
+import com.example.BackendMIIT.repository.DirectionRepository;
 import com.example.BackendMIIT.repository.ExamRepository;
 import com.example.BackendMIIT.service.ExamService;
 import org.springframework.stereotype.Service;
@@ -16,21 +21,44 @@ import java.util.stream.Collectors;
 public class ExamServiceImpl implements ExamService {
 
     private final ExamRepository examRepository;
+    private final DirectionRepository directionRepository;
+    private final DirectionExamPointsRepository depRepository;
     private final ExamMapper examMapper;
-    private final ExamParser parser;
+    private final ExamParser examParser;
+    private final DirectionExamPointsParser depParser;
 
-    public ExamServiceImpl(ExamRepository examRepository, ExamMapper examMapper, ExamParser parser) {
+    public ExamServiceImpl(ExamRepository examRepository, DirectionRepository directionRepository,
+                           DirectionExamPointsRepository depRepository,
+                           ExamMapper examMapper,
+                           ExamParser examParser,
+                           DirectionExamPointsParser depParser) {
         this.examRepository = examRepository;
+        this.directionRepository = directionRepository;
+        this.depRepository = depRepository;
         this.examMapper = examMapper;
-        this.parser = parser;
+        this.examParser = examParser;
+        this.depParser = depParser;
     }
 
     @Override
     public void parseAndSaveExamSubjects() {
-        Set<Exam> exams = parser.parseExams();
+        Set<Exam> exams = examParser.parseExams();
 
         if (getAllExams().isEmpty()) {
             examRepository.saveAll(exams);
+        }
+    }
+
+    @Override
+    public void parseAndSaveMinPoints() {
+        List<Exam> allExams = examRepository.findAll();
+        List<Direction> allDirections = directionRepository.findAll();
+
+        Set<DirectionExamPoints> directionExamPoints = depParser
+                .parseDirectionExamPoints(allExams, allDirections);
+
+        if (depRepository.findAll().isEmpty()) {
+            depRepository.saveAll(directionExamPoints);
         }
     }
 
