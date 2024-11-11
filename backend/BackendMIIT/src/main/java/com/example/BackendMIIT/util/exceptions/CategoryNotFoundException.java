2b3171b9a012c667cc2b533967b12f413c11742c
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-11 20:35:26 +0300

    refactor: replace the Category type with a String type

--- backend/BackendMIIT/src/main/java/com/example/BackendMIIT/util/exceptions/CategoryNotFoundException.java
+++ backend/BackendMIIT/src/main/java/com/example/BackendMIIT/util/exceptions/CategoryNotFoundException.java
@@ -1,10 +1,8 @@
 package com.example.BackendMIIT.util.exceptions;
 
-import com.example.BackendMIIT.model.domain.Category;
-
 public class CategoryNotFoundException extends RuntimeException {
 
-  public CategoryNotFoundException(Category category) {
-      super("Category not found: " + category.getValue());
+    public CategoryNotFoundException(String category) {
+        super("Category not found: " + category);
     }
 }
